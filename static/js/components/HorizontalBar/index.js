f7a6a58ff89e2383e6fc8d2840e99826a85fbfb0
Khmel Mykyta <hmelenok@gmail.com>
2018-04-25 19:23:43 +0300

    Some styling

--- static/js/components/HorizontalBar/index.js
+++ static/js/components/HorizontalBar/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import * as d3 from 'd3';
 import {generateArray} from '../../data-layer/array-processors';
 import {size} from 'lodash';
+import './index.scss';
 
 export default class HorizontalBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: generateArray().map(({val}) => val)
-      // data: [1, 5, 100]
-      // data: [5, 100, 300, 1000]
+      data: generateArray()
     };
   }
 
@@ -19,18 +18,16 @@ export default class HorizontalBar extends React.Component {
 
   buildTable() {
     const chart = d3.select(this.chart);
+    const numbers = this.state.data.map(({val}) => val);
     const scale = d3
       .scaleLinear()
-      .domain([0, d3.max(this.state.data)])
+      .domain([0, d3.max(numbers)])
       .range([0, window.innerWidth - 100]);
-    const p = chart.selectAll('div').data(this.state.data);
-    const axis = d3.axisBottom(scale);
-    chart
-      .append('svg')
-      .attr('height', 20)
-      .attr('width', window.innerWidth - 100)
-      .attr('class', 'x axis')
-      .call(axis);
+    const p = chart
+      .selectAll('div')
+      .data(numbers)
+      .attr('data-label', (d, index) => this.state.data[index].label);
+    this.renderAxis(chart, scale);
     p
       .enter()
       .append('div')
@@ -40,9 +37,30 @@ export default class HorizontalBar extends React.Component {
       .style('width', d => scale(d) + 'px')
       .style('height', '20px')
       .style('margin', '10px 0')
-      .style('background-color', '#0a83cf');
+      .style('background-color', '#0a83cf')
+      .attr('data-label', (d, index) => this.state.data[index].label);
 
     p.exit().remove();
+  }
+
+  renderAxis(chart, scale) {
+    const axis = d3
+      .axisBottom(scale)
+      .scale(scale)
+      .tickSizeOuter(0)
+      .tickSizeInner(0);
+    chart
+      .append('svg')
+      .attr('height', 20)
+      .attr('width', window.innerWidth - 80)
+      .attr('class', 'HorizontalBar__axis')
+      .call(axis);
+    chart.selectAll('.HorizontalBar__axis .domain').attr('stroke', 'transparent');
+    chart
+      .selectAll('.HorizontalBar__axis .tick text')
+      .style('transform', 'translate(3px, 0)')
+      .attr('fill', '#939393')
+      .style('font-size', '14px)');
   }
 
   render() {
